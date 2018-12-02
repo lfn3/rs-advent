c85b597c62ab7485230cbbca05fbfd191852770e
Liam Falconer <liam@lfn3.net>
2018-12-02 15:48:38 +0000

    Tests for problem 1

--- src/one.rs
+++ src/one.rs
@@ -2,10 +2,17 @@ use std::collections::HashSet;
 
 pub fn solve(s: &String)
 {
+    let (s1, s2) = testable_solve(s);
+    println!("Solutions are: {}, {}", s1, s2)
+}
+
+fn testable_solve(s: &String) -> (i32, i32)
+{
     let mut seen = HashSet::new();
     seen.insert(0);
 
     let mut current_freq = 0;
+    let mut solution_one = 0;
 
     for i in 0.. {
         let mut found = false;
@@ -28,7 +35,7 @@ pub fn solve(s: &String)
 //        println!("=== REPEATING ===");
 
         if i == 0 {
-            println!("Solution to part 1 is: {}", current_freq)
+            solution_one = current_freq.clone();
         }
 
         if found {
@@ -36,5 +43,14 @@ pub fn solve(s: &String)
         }
     }
 
-    println!("Solution to part 2 is: {}", current_freq)
+    (solution_one, current_freq)
+}
+
+#[test]
+fn should_solve_sample_input() {
+    assert_eq!((0, 0), testable_solve(&"+1\n-1\n".to_string()));
+    assert_eq!((4, 10), testable_solve(&"+3\n+3\n+4\n-2\n-4\n".to_string()));
+    assert_eq!((4, 5), testable_solve(&"-6\n+3\n+8\n+5\n-6\n".to_string()));
+    assert_eq!((1, 14), testable_solve(&"+7\n+7\n-2\n-7\n-4\n".to_string()));
+    assert_eq!((3, 2), testable_solve(&"+1\n-2\n+3\n+1\n".to_string()));
 }
